09a02506851ced7194c03095251fe38244150e5b
Beibarys Jmart <bumirbayev@jmart.kz>
2024-06-07 19:05:06 +0500

    update lesson naminbg

---
+++ notes.md/variable.js
@@ -0,0 +1,15 @@
+const firstName = 'John';
+const lastName = 'Doe';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+let age = 33;
+++age;
+
+age -= 1;
+
+console.log(age);
+
+
+console.log(`I am ${age} years old`);
+console.log(`My name is ${firstName}`);
