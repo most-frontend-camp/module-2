f20d8ca16ae20c425f3ad8c7f70ecb9dad678db2
Beibarys Jmart <bumirbayev@jmart.kz>
2024-06-12 20:12:33 +0500

    update

---
+++ js-statements-operators/app.js
@@ -0,0 +1,12 @@
+// comparison operators
+// return boolean value - true, false
+
+console.log(5 > 3);
+console.log(5 < 3);
+console.log(5 >= 3);
+console.log(5 <= 3);
+console.log(5 === 3);
+console.log(5 !== 3);
+console.log(5 === '5');
+
+console.log(NaN === NaN);
---
+++ js-statements-operators/compare-types.js
@@ -0,0 +1,17 @@
+console.log(5 === '5');
+console.log(5 !== '5');
+
+// ===, !== - строгие операторы
+
+console.log(5 == '5');
+console.log(5 != '5');
+
+// ==, != - нестрогие операторы
+// неожиданные не очевидные результаты, лучше их не применять
+
+console.log(true == 1);
+console.log(false == 0);
+console.log(true >false);  // 1 > 0
+
+
+console.log('000' == 0);
